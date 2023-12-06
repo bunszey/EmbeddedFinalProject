e1559a147de98aa08fde6b2cffcfb2f796ad9792
Jonasskovbakke <jonasskovbakke@gmail.com>
2023-12-06 16:24:47 +0100

    init commit

---
+++ dynamixel_sdk_image/src/image_subscriber.cpp
@@ -0,0 +1,59 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <chrono>
+#include <filesystem>
+#include <sys/stat.h>
+
+class ImageSubscriber : public rclcpp::Node
+{
+	public:
+		ImageSubscriber() : Node("image_subscriber") {
+			RCLCPP_INFO(this->get_logger(), "Initializing ImageSubscriber node");
+
+			RCLCPP_INFO(this->get_logger(), "Starting camera subscription");
+
+			camera_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+					"/image_raw",
+					10,
+					std::bind(&ImageSubscriber::onImageMsg, this, std::placeholders::_1)
+			);
+		
+			directory = std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + "/";
+			mkdir(directory.c_str(), 0777);
+
+		}
+
+	private:
+		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_subscription_;
+
+		int i = 0;
+        std::string directory;
+
+        void onImageMsg(const sensor_msgs::msg::Image::SharedPtr msg) 
+		{
+			RCLCPP_INFO(this->get_logger(), "Received image!");
+
+			cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+			
+			cv::Mat img_rgb;
+			cv::cvtColor(cv_ptr->image, img_rgb, cv::COLOR_YUV2RGB_YUYV);
+
+			std::string filename = directory + "img" + std::to_string(i++) + ".png";
+			cv::imwrite(filename, img_rgb);
+		}
+
+};
+
+int main(int argc, char *argv[])
+{
+	setvbuf(stdout,NULL,_IONBF,BUFSIZ);
+
+	rclcpp::init(argc,argv);
+	rclcpp::spin(std::make_shared<ImageSubscriber>());
+
+	rclcpp::shutdown();
+	return 0;
+}
