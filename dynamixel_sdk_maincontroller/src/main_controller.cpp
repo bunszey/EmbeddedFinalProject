2f7e4c0eb6a370a1ad702231263ad5c3dd9bbc06
bunszey <bunszey@gmail.com>
2023-12-14 10:19:11 +0100

    Added multiple thread callbacks

--- dynamixel_sdk_maincontroller/src/main_controller.cpp
+++ dynamixel_sdk_maincontroller/src/main_controller.cpp
@@ -23,25 +23,32 @@ class MainController : public rclcpp::Node
 		MainController() : Node("main_controller") {
 			RCLCPP_INFO(this->get_logger(), "Initializing MainController node");
 
+			client_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+        	sub_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+			cam_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+
+			rclcpp::SubscriptionOptions options_sub;
+			options_sub.callback_group = sub_cb_group_;
+			rclcpp::SubscriptionOptions options_camsub;
+			options_camsub.callback_group = cam_cb_group_;
+
 			RCLCPP_INFO(this->get_logger(), "Starting position subscription");
-			subscription_ = this->create_subscription<std_msgs::msg::Int32>(
-      			"gotopos", 10, std::bind(&MainController::gotorequest_callback, this, _1));
+			subscription_ = this->create_subscription<std_msgs::msg::Int32>("gotopos", 10, std::bind(&MainController::gotorequest_callback, this, _1), options_sub);
 			RCLCPP_INFO(this->get_logger(), "Starting position publisher");
 			publisher_ = this->create_publisher<dynamixel_sdk_custom_interfaces::msg::SetPosition>("set_position", 10);
 			RCLCPP_INFO(this->get_logger(), "Starting client for getting position");
-			client_ = this->create_client<dynamixel_sdk_custom_interfaces::srv::GetPosition>("get_position");
+			client_ = this->create_client<dynamixel_sdk_custom_interfaces::srv::GetPosition>("get_position", rmw_qos_profile_services_default, client_cb_group_);
 			RCLCPP_INFO(this->get_logger(), "Starting camera subscriber");
-			camera_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-					"/image_raw",
-					10,
-					std::bind(&MainController::onImageMsg, this, std::placeholders::_1)
-			);
+			camera_subscription_ = this->create_subscription<sensor_msgs::msg::Image>("/image_raw", 10,	std::bind(&MainController::onImageMsg, this, std::placeholders::_1), options_camsub);
 
 			directory = std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + "/";
 			mkdir(directory.c_str(), 0777);
 		}
 
 	private:
+		rclcpp::CallbackGroup::SharedPtr client_cb_group_;
+		rclcpp::CallbackGroup::SharedPtr sub_cb_group_;
+		rclcpp::CallbackGroup::SharedPtr cam_cb_group_;
 		rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
 		rclcpp::Publisher<dynamixel_sdk_custom_interfaces::msg::SetPosition>::SharedPtr publisher_;
 		rclcpp::Client<dynamixel_sdk_custom_interfaces::srv::GetPosition>::SharedPtr client_;
@@ -91,8 +98,12 @@ class MainController : public rclcpp::Node
 				}
 
 				auto result = client_->async_send_request(request);
-				// Wait for the result.
-				pos = result.get()->position;
+				std::future_status status = result.wait_for(1s);
+				if (status == std::future_status::ready) {
+					RCLCPP_INFO(this->get_logger(), "Received response");
+					pos = result.get()->position;
+				}
+				
 			}
 
 			std::string filename = directory + "img" + std::to_string(i++) + "_" + std::to_string(pos) +".png";
@@ -108,10 +119,15 @@ class MainController : public rclcpp::Node
 int main(int argc, char *argv[])
 {
 	setvbuf(stdout,NULL,_IONBF,BUFSIZ);
+	rclcpp::init(argc, argv);
+    auto client_node = std::make_shared<MainController>();
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(client_node);
 
-	rclcpp::init(argc,argv);
-	rclcpp::spin(std::make_shared<MainController>());
+    RCLCPP_INFO(client_node->get_logger(), "Starting client node, shut down with CTRL-C");
+    executor.spin();
+    RCLCPP_INFO(client_node->get_logger(), "Keyboard interrupt, shutting down.\n");
 
-	rclcpp::shutdown();
+    rclcpp::shutdown();
 	return 0;
 }
